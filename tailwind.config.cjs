d7bc49ca345981526c26e5f512bf01ef6487de4e
Ben Brown <9870007+brownben@users.noreply.github.com>
2024-03-25 11:02:35 +0000

    add dark mode
    - add github logo to link
    - left align for bigger screens
    - update munro stats

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -6,7 +7,16 @@ module.exports = {
       sans: ['Geist Var', 'Inter var', 'system-ui'],
     },
     extend: {},
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      red: colors.red,
+      blue: colors.blue,
+    },
   },
   plugins: [],
 }
-
