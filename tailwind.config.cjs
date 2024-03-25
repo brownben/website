81212074eb34a03db05553f7d05c01009420d766
Ben Brown <9870007+brownben@users.noreply.github.com>
2024-03-25 10:54:46 +0000

    Change to Geist font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      sans: ['Inter var', 'system-ui'],
+      sans: ['Geist Var', 'Inter var', 'system-ui'],
     },
     extend: {},
   },
