401130c9ad6c6b2046555077431c8952f937c4e5
Ben Brown <9870007+brownben@users.noreply.github.com>
2024-03-26 12:39:23 +0000

    add post layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,6 @@ module.exports = {
     fontFamily: {
       sans: ['Geist Var', 'Inter var', 'system-ui'],
     },
-    extend: {},
 
     colors: {
       transparent: 'transparent',
@@ -17,6 +16,48 @@ module.exports = {
       red: colors.red,
       blue: colors.blue,
     },
+
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            img: {
+              borderRadius: theme('borderRadius.lg'),
+              height: 'auto',
+              width: 'auto',
+              maxWidth: '100%',
+              maxHeight: '16rem',
+            },
+
+            a: {
+              color: 'var(--tw-prose-body)',
+              textDecorationColor: theme('colors.gray.300'),
+              fontWeight: 'normal',
+              textDecorationThickness: '1.5px',
+              textUnderlineOffset: '2px',
+              textDecorationColor: theme('colors.gray.300'),
+              transition: 'text-decoration-color 100ms',
+
+              '&:hover': {
+                textDecorationColor: theme('colors.blue.500'),
+              },
+
+              '@media (prefers-color-scheme: dark)': {
+                textDecorationColor: theme('colors.gray.500'),
+              },
+            },
+
+            'blockquote p::after': {
+              content: '""',
+            },
+            'blockquote p::before': {
+              content: '""',
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+
+  plugins: [require('@tailwindcss/typography')],
 }
